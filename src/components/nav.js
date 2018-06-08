7c5fa20c7bbd5975d7bf0f2ab94dc68bd8e273c9
William Whatley <william@catatek.com>
2018-06-07 22:38:42 -0400

    Finished styling for Nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,22 +1,83 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
-import { Title1, Title2, Title3 } from '../theme/index'
+import { Title1, Title3, Column, Text } from '../theme/index'
+import Headshot from '../assets/william.png'
+import Medium from '../assets/medium.svg'
+import Github from '../assets/github.svg'
+import Linkedin from '../assets/linkedin.svg'
 
-const Row = styled.div`
-    display: flex;
-    flex-direction: column;
+const StyledColumn = styled(Column)`
     height: 100vh;
     width: 300px;
     background-color: #3D4C54;
 `
 
+const Img = styled.img`
+  width: 250px;
+  height: 250px;
+`
+
+const Row = styled.div`
+  display: flex;
+  justify-content: ${props => props.justifycontent};
+  margin: ${props => props.margin};
+  width: ${props => props.width};
+`
+
+const Title2 = styled.h2`
+font-family: 'Fira Sans', sans-serif;
+font-size: 1.4em;
+color: #ECC090;
+margin: 0;
+`
+const StyledLink = styled(Link)`
+  font-size: 1em;
+  font-family: 'Fira Sans', sans-serif;
+  font-weight: 300;
+  color: #8DB5EA;
+  text-decoration: none;
+  margin: .4em 0;
+  letter-spacing: .1em;
+`
+
+const Icon = styled.img`
+  width: 30px;
+  height: 30px;
+`
+
 const Nav = () => (
-  <Row>
-    <Title1>Hey</Title1>
-    <Title2>Hey</Title2>
-    <Title3>Hey</Title3>
-  </Row>
+  <StyledColumn>
+    <Row justifycontent="center" margin="1em 0 0 0">
+      <Img src={Headshot} alt="Will's Headshot" />
+    </Row>
+    <Column alignitems="center" margin="0 0 1em 0">
+      <Title3 margin=".8em 0 .2em 0">Personal Blog & Site of</Title3>
+      <Title2 margin=".2em 0">William W. Whatley</Title2>
+    </Column>
+    <Column margin="0 2.2em">
+      <StyledLink to="/test" activeStyle={{ color: 'red' }}>
+        Home
+      </StyledLink>
+      <StyledLink to="/test" activeStyle={{ color: 'red' }}>
+        About
+      </StyledLink>
+      <StyledLink to="/test" activeStyle={{ color: 'red' }}>
+        What am I doing now?
+      </StyledLink>
+      <StyledLink to="/test" activeStyle={{ color: 'red' }}>
+        Posts
+      </StyledLink>
+      <StyledLink to="/test" activeStyle={{ color: 'red' }}>
+        My Tools
+      </StyledLink>
+    </Column>
+    <Row margin="2em auto" justifycontent="space-evenly" width="80%">
+      <Icon src={Medium} alt="Will's Medium" />
+      <Icon src={Github} alt="Will's Github" />
+      <Icon src={Linkedin} alt="Will's Linkedin" />
+    </Row>
+  </StyledColumn>
 )
 
 export default Nav
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,18 +1,34 @@
 import styled from 'styled-components'
 
 export const Title1 = styled.h1`
-    font-family: 'Fire Sans', sans-serif;
+    font-family: 'Fira Sans', sans-serif;
     font-size: 2em;
+    font-weight: 600;
     color: #ECC090;
 `
 
 export const Title2 = styled.h2`
-    font-family: 'Fire Sans', sans-serif;
+    margin: ${props => props.margin || '.5em 0'}
+    font-family: 'Fira Sans', sans-serif;
     font-size: 1.6em;
     color: #7FC1CA;
 `
 
 export const Title3 = styled.h3`
-    color: #808080;
+    margin: ${props => props.margin || '.5em 0'}
+    color: #5882A0;
     font-size: 1.2em;
+    font-family: 'Fira Sans', sans-serif;
+    font-weight: 300;
+`
+
+export const Text = styled.p`
+    
+`
+
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: ${props => props.margin}
+    align-items: ${props => props.alignitems};
 `
